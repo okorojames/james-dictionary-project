865d61920a5c37a06c4a6068d261f6bad90f46bd
okorojames <okorojameschizaram@gmail.com>
2023-02-26 10:01:32 +0100

    First section
    I'm done with the first section of the project, though not yet done with the functionality, since i haven't fetched the data that am going to display.

--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const FirstSection = () => {
   return (
-    <div>FirstSection</div>
+    <div className='firstSectionContainer'>
+      <div className="firstSecText">
+        <p className="first--sec--p--one">{'Input Value'}</p>
+        <p className="first--sec--p--two">{'/value translate/'}</p>
+      </div>
+      <i className="ri-volume-up-line first--sec--icon"></i>
+    </div>
   )
 }
 
